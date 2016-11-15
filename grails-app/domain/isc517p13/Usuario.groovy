e684cb7454f25c36097ecfec549938cf9c7ec0de
Carlos <carlosbatista108@hotmail.com>
2016-11-14 23:12:12 -0400

    clase Usuario

---
+++ grails-app/domain/isc517p13/Usuario.groovy
@@ -0,0 +1,20 @@
+package isc517p13
+
+class Usuario {
+
+    String nombre
+    String contrasena
+    boolean isAdmin
+
+
+
+    static belongsTo = [departmento: Departamento]
+
+    static constraints = {
+
+        nombre (blank: false)
+        contrasena (blank: false)
+        departmento (nullable: true)
+
+    }
+}
